d3dfa77c7e6deeeb18831179c844beb232b05391
Mschm <mjs6409@g.rit.edu>
2020-10-29 23:10:07 -0400

    Finished the ReportingStructure. Still has the bug though, so all report as having 0.

---
+++ code-challenge/Controllers/ReportingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+    [Route("api/employee/reports")]
+    public class ReportingController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IReportingStructureService _reportingService;
+
+        public ReportingController(ILogger<EmployeeController> logger, IReportingStructureService reportingService)
+        {
+            _logger = logger;
+            _reportingService = reportingService;
+        }
+
+        [HttpGet("{id}", Name = "getReportingStructureById")]
+        public IActionResult getReportingStructureById(String id)
+        {
+            _logger.LogDebug($"Received reporting structure get request for '{id}'");
+
+            var reportingStructure = _reportingService.GetById(id);
+
+            if (reportingStructure == null)
+                return NotFound();
+
+            return Ok(reportingStructure);
+        }
+    }
+}
---
+++ code-challenge/Services/ReportingStructureService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using challenge.Repositories;
+
+namespace challenge.Services
+{
+    public class ReportingStructureService : IReportingStructureService
+    {
+        private readonly IEmployeeRepository _employeeRepository;   // Needed for getting the employee that will be added into the ReportingStructure.
+        private readonly ILogger<EmployeeService> _logger;          // Logs all of the necessary debug statements.
+
+        public ReportingStructureService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Creates a ReportingStructure at the time of request by the given employee id.
+        /// </summary>
+        /// <param name="id"> The employee's id that will be used to fill out the ReportingStructure. </param>
+        /// <returns> A newly created ReportingStructure with the employee and how many reports they have. </returns>
+        public ReportingStructure GetById(string id)
+        {
+            if(!String.IsNullOrEmpty(id))
+            {
+                ReportingStructure reportingStructure = new ReportingStructure();
+
+                // Getting the employee in order to add it to the struture
+                var employee = _employeeRepository.GetById(id);
+
+                reportingStructure.employee = employee;
+
+                int reports = GetDirectReports(employee);
+
+                reportingStructure.numberOfReports = reports;
+
+                return reportingStructure;
+            }
+
+            return null;
+        }
+
+        private int GetDirectReports(Employee employee)
+        {
+            var reportsList = employee.DirectReports;
+
+            int reports = 0;
+
+            if (reportsList != null)
+            {
+                if (reportsList.Count == 0)
+                {
+                    foreach (Employee e in reportsList)
+                    {
+                        reports += 1 + GetDirectReports(e);
+                    }
+                }
+            }
+
+            return reports;
+        }
+    }
+}
